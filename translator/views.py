d39be5baac4d59525d7f55b47e10d91de1b21cbd
anetapec <aneta.pecka@gmail.com>
2023-09-25 20:41:42 +0200

    changing letters to uppercase

--- translator/views.py
+++ translator/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def translator_view(request):
-    return render(request, 'translator.html')
+def translator_view(request): 
+    
+    if request.method == 'POST':
+        orginal_text = request.POST['my_textarea']
+        output = orginal_text.upper()
+        return render(request, 'translator.html', {'output_text': output, 'orginal_text': orginal_text})
+    else:
+        return render(request, 'translator.html')
